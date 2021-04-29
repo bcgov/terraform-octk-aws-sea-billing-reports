f993bca64843a5b04d726825b683872c6aa19552
Shea Phillips <shea.phillips@gmail.com>
2021-04-29 16:16:56 -0700

    - added additional metadata columns to summarized data and reports

--- local/summarize_charges.py
+++ local/summarize_charges.py
@@ -15,28 +15,34 @@ def read_file_into_dataframe(local_file):
 
 def enhance_with_metadata(df, team_index_by_account_id):
 	# Account ID	Service Name	CITZ Internal Project Code	Owner Name	Account Name	Account Owner Email	Account Status
-	df['Team'] = df['line_item_usage_account_id'].apply(lambda x: team_index_by_account_id.get(x, "Core"))
-	# df['Team'] = df['line_item_usage_account_id'].apply(lambda x: team_index_by_account_id.get(x['business_unit'], "Core"))
-
+	# df['Team'] = df['line_item_usage_account_id'].apply(lambda x: team_index_by_account_id.get(x, "Core"))
+	core_team = {
+		"business_unit": "SEA Core",
+		"contact_email": "julian.subda@gov.bc.ca",
+		"contact_name": "Julian Subda",
+		"name": "NA"
+	}
+	df['Team'] = df['line_item_usage_account_id'].apply(lambda x: team_index_by_account_id.get(x, core_team)['business_unit'])
+	df['Owner_Name'] = df['line_item_usage_account_id'].apply(lambda x: team_index_by_account_id.get(x, core_team)['contact_name'])
+	df['Owner_Email'] = df['line_item_usage_account_id'].apply(lambda x: team_index_by_account_id.get(x, core_team)['contact_email'])
+	df['Account_Name'] = df['line_item_usage_account_id'].apply(lambda x: team_index_by_account_id.get(x, core_team)['name'])
 
 
 def make_team_lookup(teams):
 	team_index_by_account_id = {}
+	team_details_by_account_id = {}
+
+	for team in teams:
+		for details in team['account_details']:
+			team_details_by_account_id[details['id']] = details
 
 	for team in teams:
 		for account_id in team['accountIds']:
-			team_index_by_account_id[account_id] = team['business_unit']
+			team_details = team_details_by_account_id[account_id]
+			team.update(team_details)
+			team_index_by_account_id[account_id] = team
 
 	return team_index_by_account_id
-
-# def make_team_lookup(teams):
-# 	team_index_by_account_id = {}
-#
-# 	for team in teams:
-# 		for account_id in team['accountIds']:
-# 			team_index_by_account_id[account_id] = team
-#
-# 	return team_index_by_account_id
 
 
 def report(query_results_file, report_output_path, query_parameters):
@@ -82,11 +88,14 @@ def aggregate(query_results_file, query_parameters, summary_output_file):
 	enhance_with_metadata(df, team_index_by_account_id)
 
 	df = df.groupby(
-		['year', 'month', 'line_item_usage_account_id', 'Team', 'line_item_product_code']).sum().reset_index()
+		['year', 'month', 'line_item_usage_account_id', 'Account_Name', 'Team', 'Owner_Name', 'Owner_Email', 'line_item_product_code']).sum().reset_index()
+
+	fieldnames = ['Year', 'Month', 'Account ID', 'Account Name', 'Billing Group', 'Owner Name', 'Owner Email', 'AWS Service']
 
 	with open(summary_output_file, 'w', newline='') as csvfile:
-		line_item_writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+		line_item_writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+		line_item_writer.writerow(fieldnames)
 
 		for index, row in df.iterrows():
-			line_item_writer.writerow([row['year'], row['month'], row['line_item_usage_account_id'], row['Team'],
+			line_item_writer.writerow([row['year'], row['month'], row['line_item_usage_account_id'], row['Account_Name'], row['Team'], row['Owner_Name'], row['Owner_Email'],
 									   row['line_item_product_code'], "{:10.4f}".format(row['line_item_blended_cost'])])
