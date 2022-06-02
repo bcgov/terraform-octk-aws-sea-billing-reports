4c42f5762f7ce06dd9b2070d044d62554c514ceb
unoah <13597351+unoah@users.noreply.github.com>
2022-06-02 12:11:29 -0700

    Clean up on start/end dates for quarterly and monthly functions. Initial setup for manual billing report generation.

--- billing-report-utility/billing-cpf-1068.py
+++ billing-report-utility/billing-cpf-1068.py
@@ -5,6 +5,8 @@ import logging
 import requests
 
 from datetime import date, datetime, timezone, timedelta, tzinfo
+from fiscalyear import FiscalMonth, FiscalQuarter, setup_fiscal_calendar
+
 from BillingManager import BillingManager
 
 logger = logging.getLogger(__name__)
@@ -13,6 +15,8 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler = logging.StreamHandler(sys.stdout)
 handler.setFormatter(formatter)
 logger.addHandler(handler)
+
+setup_fiscal_calendar(start_month=4)
 
 
 def weekly(event_bridge_params):
@@ -40,58 +44,22 @@ def weekly(event_bridge_params):
 	bill_manager.do()
 
 
+'''
+EventBridge schedule is set to trigger the monthly report function on the first
+day of each month. The generated report is meant to be for the previous month
+e.g.: EventBridge trigger on Feb 01 should generate report for Jan 01.
+'''
+
+
 def monthly(event_bridge_params):
 	logger.info(f"Called monthly function")
 	logger.info(f"event_bridge_params: {json.dumps(dict(event_bridge_params))}")
 
+	previous_fiscal_month_start = FiscalMonth.current().prev_fiscal_month.start.strftime("%Y, %m, %d, %H, %M, %S")
+	previous_fiscal_month_end = FiscalMonth.current().prev_fiscal_month.end.strftime("%Y, %m, %d, %H, %M, %S")
 
-def quarterly(event_bridge_params):
-	logger.info(f"Called quarterly function")
-	logger.info(f"event_bridge_params: {json.dumps(dict(event_bridge_params))}\n")
-
-	start_date = date.today()
-	end_date = date.today()
-
-	quarter = ((datetime.now().month - 1) // 3 + 1)
-
-	print(f"Which Quarter: {quarter}\n")
-
-	if quarter == 1:
-		start_date = datetime.combine(
-			date(datetime.datetime.now().year, 4, 1),
-			datetime.min.time(), tzinfo=timezone.utc
-		)
-		end_date = datetime.combine(
-			date(datetime.datetime.now().year, 7, 1),
-			datetime.min.time(), tzinfo=timezone.utc
-		)
-	elif quarter == 2:
-		start_date = datetime.combine(
-			date(datetime.now().year, 7, 1),
-			datetime.min.time(), tzinfo=timezone.utc
-		)
-		end_date = datetime.combine(
-			date(datetime.now().year, 10, 1),
-			datetime.min.time(), tzinfo=timezone.utc
-		)
-	elif quarter == 3:
-		start_date = datetime.combine(
-			date(datetime.now().year, 10, 1),
-			datetime.min.time(), tzinfo=timezone.utc
-		)
-		end_date = datetime.combine(
-			date(datetime.now().year + 1, 1, 1),
-			datetime.min.time(), tzinfo=timezone.utc
-		)
-	elif quarter == 4:
-		start_date = datetime.combine(
-			date(datetime.now().year, 1, 1),
-			datetime.min.time(), tzinfo=timezone.utc
-		)
-		end_date = datetime.combine(
-			date(datetime.now().year, 4, 1),
-			datetime.min.time(), tzinfo=timezone.utc
-		)
+	start_date = datetime.strptime(previous_fiscal_month_start, "%Y, %m, %d, %H, %M, %S")
+	end_date = datetime.strptime(previous_fiscal_month_end, "%Y, %m, %d, %H, %M, %S")
 
 	event_bridge_params.update({
 		"carbon_copy": None,
@@ -99,32 +67,107 @@ def quarterly(event_bridge_params):
 		"start_date": start_date,
 		"end_date": end_date
 	})
+	logger.info(f"event_bridge_params_updated: {event_bridge_params}\n")
 
-	logger.info(f"event_bridge_params_updated: {json.dumps(dict(event_bridge_params))}\n")
+	bill_manager = BillingManager(event_bridge_params)
+	bill_manager.do()
 
-	# bill_manager = BillingManager(event_bridge_params)
-	# bill_manager.do()
+
+'''
+EventBridge schedule is set to trigger the quarterly report function on the first
+day of each quarter. The generated report is meant to be for the previous quarter
+e.g.: EventBridge trigger at start of Q1 2022 should generate report for Q4 2021.
+
+Fiscal year begins April 01 of the calendar year. As a result, the second quarter 
+of the calendar year is the first quarter of the fiscal year. The mapping between
+calendar quarter and fiscal quarter is as follows:
+
+Calendar Q1 <--> Fiscal Q4 <--> Jan - Mar 
+Calendar Q2 <--> Fiscal Q1 <--> Apr - Jun
+Calendar Q3 <--> Fiscal Q2 <--> Jul - Sep
+Calendar Q4 <--> Fiscal Q3 <--> Oct - Dec
+'''
+
+
+def quarterly(event_bridge_params):
+	logger.info(f"Called quarterly function")
+	logger.info(f"event_bridge_params: {json.dumps(dict(event_bridge_params))}\n")
+
+	previous_fiscal_quarter_start = FiscalQuarter.current().prev_fiscal_quarter.start.strftime("%Y, %m, %d, %H, %M, %S")
+	previous_fiscal_quarter_end = FiscalQuarter.current().prev_fiscal_quarter.end.strftime("%Y, %m, %d, %H, %M, %S")
+
+	start_date = datetime.strptime(previous_fiscal_quarter_start, "%Y, %m, %d, %H, %M, %S")
+	end_date = datetime.strptime(previous_fiscal_quarter_end, "%Y, %m, %d, %H, %M, %S")
+
+	event_bridge_params.update({
+		"carbon_copy": None,
+		"billing_groups": None,
+		"start_date": start_date,
+		"end_date": end_date
+	})
+	logger.info(f"event_bridge_params_updated: {event_bridge_params}\n")
+
+	bill_manager = BillingManager(event_bridge_params)
+	bill_manager.do()
+
+
+# TODO: Move below comments to README
+''' 
+Used to generate report for a specific date/time period. This function will not be associated
+with an EventBridge target. Executing this function requires credentials from the corresponding
+LZ Operations account and the following environment variables:
+
+REPORT_TYPE="Manual"
+START_DATE=<Target start date>
+END_DATE=<Target end data>
+DELIVER=True|False
+RECIPIENT_OVERRIDE="hello.123@localhost"
+ATHENA_QUERY_ROLE_TO_ASSUME_ARN="arn:aws:iam::<LZ#-ManagementAccountID>:role/BCGov-Athena-Cost-and-Usage-Report"
+ATHENA_QUERY_DATABASE="athenacurcfn_cost_and_usage_report"
+QUERY_ORG_ACCOUNTS_ROLE_TO_ASSUME_ARN="arn:aws:iam::<LZ#-ManagementAccountID>:role/BCGov-Query-Org-Accounts"
+ATHENA_QUERY_OUTPUT_BUCKET="bcgov-ecf-billing-reports-output-<LZ#-ManagementAccountID>-ca-central-1"
+ATHENA_QUERY_OUTPUT_BUCKET_ARN="arn:aws:s3:::bcgov-ecf-billing-reports-output-<LZ#-ManagementAccountID>-ca-central-1"
+CMK_SSE_KMS_ALIAS="arn:aws:kms:ca-central-1:<LZ#-ManagementAccountID>:alias/BCGov-BillingReports"
+'''
+
+
+def manual(event_bridge_params):
+	logger.info(f"Called manual function")
+	logger.info(f"event_bridge_params: {json.dumps(dict(event_bridge_params))}\n")
+
+	start_date = datetime.strptime(os.environ["START_DATE"], "%Y, %m, %d, %H, %M, %S")
+	end_date = datetime.strptime(os.environ["END_DATE"], "%Y, %m, %d, %H, %M, %S")
+
+	event_bridge_params.update({
+		"carbon_copy": None,
+		"billing_groups": None,
+		"start_date": start_date,
+		"end_date": end_date
+	})
+	logger.info(f"event_bridge_params_updated: {event_bridge_params}\n")
+
+	bill_manager = BillingManager(event_bridge_params)
+	bill_manager.do()
 
 
 def main():
 	print("Cloud Pathfinder Billing Utility!")
 
-	# logger.info(f"Environment Variables: {json.dumps(dict(os.environ))}")
-	#
-	# metadata_uri_v4 = os.environ["ECS_CONTAINER_METADATA_URI_V4"]
-	# get_v4_metadata = requests.get(format(metadata_uri_v4))
-	# v4_metadata = get_v4_metadata.json()
-	# logger.info(f"V4 Metadata: {json.dumps(v4_metadata)}")
+	logger.info(f"Environment Variables: {json.dumps(dict(os.environ))}")
+
+	metadata_uri_v4 = os.environ["ECS_CONTAINER_METADATA_URI_V4"]
+	get_v4_metadata = requests.get(format(metadata_uri_v4))
+	v4_metadata = get_v4_metadata.json()
+	logger.info(f"V4 Metadata: {json.dumps(v4_metadata)}")
 
 	event_bridge_payload = {
 		"report_type": os.environ["REPORT_TYPE"].lower(),
 		"deliver": bool(os.environ["DELIVER"]),
 		"recipient_override": os.environ["RECIPIENT_OVERRIDE"].lower()
 	}
+	logger.info(f"event_bridge_payload: {json.dumps(dict(event_bridge_payload))}")
 
-	report_type = globals()[event_bridge_payload['report_type']](event_bridge_payload)
-
-	# logger.info(f"event_bridge_payload: {json.dumps(dict(event_bridge_payload))}")
+	globals()[event_bridge_payload['report_type']](event_bridge_payload)
 
 
 if __name__ == "__main__":
